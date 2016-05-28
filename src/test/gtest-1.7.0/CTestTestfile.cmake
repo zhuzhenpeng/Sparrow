f9a68cc5eb95eebe0b4f984176a18ec54a452f24
zhuzhenpeng <522897726@qq.com>
2016-05-28 21:32:39 +0800

    remove useless test

--- src/test/gtest-1.7.0/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zhu/wp/miniC/src/test/gtest-1.7.0
-# Build directory: /home/zhu/wp/miniC/src/test/gtest-1.7.0
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
